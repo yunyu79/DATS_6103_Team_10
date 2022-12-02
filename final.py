50fb4980403c764633686e7d3e284486d5c1f6e8
jiwoosuh <jiwoosuh@gwu.edu>
2022-12-01 21:13:15 -0500

    update
    I made changes on the introductory plots
    and did the EDA for cost/customer care calls which are assigned to me

--- final.py
+++ final.py
@@ -47,9 +47,135 @@ print(sns.boxplot(data['Discount_offered']))
 #%%
 print(sns.boxplot(data['Weight_in_gms']))
 
+#%%
+# Pairplots for integer columns (Initial EDA)
+integer_cols = data.select_dtypes(include = ['int64'])
+print(integer_cols.head())
+intcols = integer_cols.columns
 # %%
-import matplotlib.pyplot as plt
+# counts plots for every column
+cols = ['Warehouse_block', 'Mode_of_Shipment', 'Customer_care_calls', 'Customer_rating', 'Prior_purchases', 'Product_importance', 'Gender', 'Reached.on.Time_Y.N']
+plt.figure(figsize = (16, 20))
+sns.set_theme(style="ticks", palette="pastel")
+nplot = 1
+for i in range(len(cols)):
+    if nplot <= len(cols):
+        ax = plt.subplot(4, 2, nplot)
+        sns.countplot(x = cols[i], data = data, ax = ax)
+        plt.title(f"\n{cols[i]} Counts", fontsize = 15)
+        nplot += 1
+plt.show()
+#%%
+# boxplots for On-time delivery 
+intcols21 = intcols.drop(["Reached.on.Time_Y.N"])
+plt.figure(figsize = (16, 20))
+sns.set_theme(style="ticks", palette="pastel")
+nplot = 1
+for i in range(len(intcols21)):
+    if nplot <= len(intcols21):
+        ax = plt.subplot(4, 2, nplot)
+        sns.boxplot(y = intcols21[i], x= "Reached.on.Time_Y.N", data = data, ax = ax)
+        plt.title("Boxplots for On-time delivery by "f"{intcols21[i]}" , fontsize = 13)
+        nplot += 1
+plt.show()
+
+# %%
+# Correlation Matrix
 plt.figure(figsize = (18, 7))
 sns.heatmap(data.corr(), annot = True, fmt = '0.2f', annot_kws = {'size' : 15}, linewidth = 5, linecolor = 'orange')
 plt.show()
+
+# %%
+# Histogram for Cost
+sns.histplot(data, x = "Cost_of_the_Product", hue = "Reached.on.Time_Y.N", multiple = "stack")
+plt.title("Histogram for Cost of the Product")
+plt.show()
+
+# %%
+# Boxplots for Object columns and Cost 
+object_cols = data.select_dtypes(include = ['object'])
+object_cols.head()
+obcol = object_cols.columns
+
+plt.figure(figsize = (16, 20))
+sns.set_theme(style="ticks", palette="pastel")
+nplot = 1
+for i in range(len(obcol)):
+    if nplot <= len(obcol):
+        ax = plt.subplot(4, 2, nplot)
+        sns.boxplot(x = obcol[i], y= "Cost_of_the_Product", hue = "Reached.on.Time_Y.N", data = data, ax = ax)
+        plt.title("Boxplot for Cost of the Product by "f"{obcol[i]}" , fontsize = 13)
+        nplot += 1
+plt.show()
+# for boxplots there were no significant differences between x variables but there are differences by whether the product was reached on time
+
+# %%
+# violins for customer calls and ratings 
+intcols2 = intcols.drop(["Cost_of_the_Product", "Reached.on.Time_Y.N"])
+intcols22 = intcols2[:2]
+plt.figure(figsize = (16, 20))
+sns.set_theme(style="ticks", palette="pastel")
+nplot = 1
+for i in range(len(intcols22)):
+    if nplot <= len(intcols22):
+        ax = plt.subplot(4, 2, nplot)
+        sns.violinplot(x = intcols22[i], y= "Cost_of_the_Product", hue = "Reached.on.Time_Y.N", data = data, ax = ax, split = True)
+        plt.title("Violinplots for Cost of the Product by "f"{intcols22[i]}" , fontsize = 13)
+        nplot += 1
+plt.show()
+# the more customers call, the higher its cost
+
+#%%
+# KDE plot for cost and weight
+sns.displot(data=data, x="Cost_of_the_Product", y="Weight_in_gms", hue="Reached.on.Time_Y.N", kind = "kde", multiple="fill", clip=(0, None))
+plt.title("KDE plot for Cost of the product by Weight")
+plt.show()
+
+# %%
+# violins for customer calls 
+plt.figure(figsize = (16, 20))
+sns.set_theme(style="ticks", palette="pastel")
+nplot = 1
+for i in range(len(intcols22)):
+    if nplot <= len(intcols22):
+        ax = plt.subplot(4, 2, nplot)
+        sns.violinplot(x = intcols22[i], y= "Cost_of_the_Product", hue = "Reached.on.Time_Y.N", data = data, ax = ax, split = True)
+        plt.title("Violinplots for Cost of the Product by "f"{intcols22[i]}" , fontsize = 13)
+        nplot += 1
+plt.show()
+
+# %%
+# Histogram for Customer Calls
+sns.histplot(data, x = "Customer_care_calls", hue = "Reached.on.Time_Y.N", multiple = "dodge", bins=10)
+plt.title("Histogram for Customer Calls")
+plt.show()
+
+# %%
+# Violinplots for Customer Calls by Ontime
+plt.figure(figsize = (16, 20))
+sns.set_theme(style="ticks", palette="pastel")
+nplot = 1
+for i in range(len(obcol)):
+    if nplot <= len(obcol):
+        ax = plt.subplot(4, 2, nplot)
+        sns.violinplot(x = obcol[i], y= "Customer_care_calls", hue = "Reached.on.Time_Y.N", data = data, ax = ax, split = True)
+        plt.title("Violinplots for Customer Calls by "f"{obcol[i]}" , fontsize = 13)
+        nplot += 1
+plt.show()
+
+# %%
+intcols3 = intcols.drop(["Customer_care_calls", "Reached.on.Time_Y.N"])
+plt.figure(figsize = (16, 20))
+sns.set_theme(style="ticks", palette="pastel")
+nplot = 1
+for i in range(len(intcols3)):
+    if nplot <= len(intcols3):
+        ax = plt.subplot(4, 2, nplot)
+        sns.boxplot(y = intcols3[i], x= "Customer_care_calls", hue = "Reached.on.Time_Y.N", data = data, ax = ax)
+        plt.title("Boxplots for Customer Care Calls by "f"{intcols3[i]}" , fontsize = 13)
+        nplot += 1
+plt.show()
+# boxenplots also works
+
+
 # %%
